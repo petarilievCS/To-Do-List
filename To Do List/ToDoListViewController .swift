2af550cd754c38ba54649619bddc24038a682fbb
Petar Iliev <petariliev2002@gmail.com>
2022-06-26 00:11:17 -0700

    Added functionality to add new tasks to list

--- To Do List/ToDoListViewController .swift
+++ To Do List/ToDoListViewController .swift
@@ -9,13 +9,15 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
     
-    let itemArray = ["Wash towel", "Clean pan", "Learn Swift"]
+    var itemArray = ["Wash towel", "Clean pan", "Learn Swift"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    //MARK: - tableView delegate methods
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
@@ -26,7 +28,7 @@ class ToDoListViewController: UITableViewController {
         return cell
     }
     
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         let accessory = tableView.cellForRow(at: indexPath)?.accessoryType
         
@@ -37,6 +39,26 @@ class ToDoListViewController: UITableViewController {
         }
         
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    //MARK: - Add new items
+    
+    @IBAction func addButtonClicked(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Task", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Task", style: .default) { action in
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { alertTextField in
+            alertTextField.placeholder = "Create new task..."
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
     
 }
